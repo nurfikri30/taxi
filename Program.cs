1fb991a5140ba10b0d337a5d3bd9d45640198aae
fikri <kriezz2202@gmail.com>
2019-03-26 01:32:34 +0700

    taxi class

---
+++ Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace taxi
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            taxi saya = new taxi();
+
+            saya.nama = "fikri";
+            saya.onduty = true;
+            saya.pass = 3f;
+
+            saya.status();
+            saya.drop();
+
+            Console.ReadKey();
+        }
+    }
+}
---
+++ taxi.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace taxi
+{
+    public class taxi
+    {
+        public string nama { get; set; }
+        public Boolean onduty { get; set; }
+        public float pass { get; set; }
+
+        public void status()
+        {
+            Console.WriteLine("nama  : {0}", nama);
+            Console.WriteLine("Onduty :{0}", onduty);
+            Console.WriteLine("pass  : {0}", pass);
+            Console.WriteLine("{0} sedang menjemput penumpang", nama);
+        }
+        public void drop()
+        {
+            Console.WriteLine("{0} sedang mengantar penumpang", nama);
+        }
+
+    }
+}
+
